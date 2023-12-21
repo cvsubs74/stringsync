0cc4a350ba11e6406a9f18d76c61c556d5094126
CV Viswanathan <csubramanian@onetrust.com>
2023-12-21 14:03:14 -0800

    Added instructions to users in the Track Recommendations Dashboard

--- dashboards/TrackRecommendationDashboard.py
+++ dashboards/TrackRecommendationDashboard.py
@@ -21,21 +21,30 @@ class TrackRecommendationDashboard:
         custom_style = "<style>h2 {font-size: 20px;}</style>"
         st.markdown(f"{custom_style}<h2>Track Recommendations</h2>", unsafe_allow_html=True)
         st.markdown("""
-            <p>Based on your recording submissions and scores, we've curated these <b>track recommendations</b> to aid in your skill development and address challenges from past sessions. Here's a guide to understanding and utilizing this information:</p>
+            <p>Based on your recording submissions and scores, we've curated these <b>track recommendations</b> to aid in your <b>skill development</b> and address challenges from past sessions. Here's a guide to understanding and utilizing this information:</p>
             <ul>
-                <li><b>Track Details</b>: Each track is accompanied by specific details. <b>Pay attention to the 'Level', 'Your Average Score', 'Track Average Score', 'Your Top Score', 'Track Top Score', and 'Track Threshold Score'</b>. These metrics provide insights into the track's difficulty and your performance in comparison to the overall and threshold scores.</li>
-                <li><b>Color Coding</b>: Colors represent your performance status.
+                <li><b>Track Details</b>: Each track is accompanied by the following details:
                     <ul>
-                        <li><span style='color: red;'><b>Red</b></span>: Indicates <b>areas needing improvement</b>, such as an average score below 80% of the threshold or high days on track.</li>
-                        <li><span style='color: green;'><b>Green</b></span>: Signifies that you are <b>on track</b>, like an average score within 80% of the threshold.</li>
+                        <li><b>Level</b>: Indicates the <b>complexity</b> and <b>skill requirement</b> of the track. It helps you select a track that matches your current ability and challenges you to grow further.</li>
+                        <li><b>Your Average Score</b>: Calculated from your <b>top 3 scoring recordings</b> for the track. This score reflects your <b>consistent performance</b> at your best and helps identify your strengths in the track.</li>
+                        <li><b>Track Average Score</b>: Derived from the <b>top 10 scoring recordings</b> across all students for that track. This provides a comparative benchmark, showing where you stand among peers and what excellence in the track looks like.</li>
+                        <li><b>Your Top Score</b>: The highest score you've achieved in a single recording for the track. It highlights your <b>peak performance</b> and potential in mastering the track's content.</li>
+                        <li><b>Track Top Score</b>: The highest score achieved by any student on the track. It serves as an <b>aspirational goal</b>, showcasing the highest level of achievement possible on the track.</li>
+                        <li><b>Track Threshold Score</b>: A predefined benchmark score for the track. Surpassing this score consistently signifies your <b>proficiency</b> and understanding of the track's material.</li>
+                        <li><b>Days on Track</b>: Represents the total number of days you have spent working on this track. It helps in assessing the <b>time you've invested</b> and could indicate if a different learning approach may be required based on the duration.</li>
+                        <li><b>Last Remark</b>: The most recent feedback or note regarding your performance on the track. It can provide specific insights and suggestions for improvement, guiding your learning process.</li>
+                        <li><b>Assessments</b>: Each track comes with a tailored assessment based on your performance. This includes <b>suggestions and observations</b> aimed at helping you understand your current standing and how to improve. <b>Read these carefully</b> to gain insights into your learning journey.</li>
+                        <li><b>Color Coding</b>: Utilized to visually represent your performance status. 
+                            <ul>
+                                <li><span style='color: red;'><b>Red</b></span>: Indicates <b>areas that need improvement</b>, such as an average score below the threshold.</li>
+                                <li><span style='color: green;'><b>Green</b></span>: Suggests <b>satisfactory performance</b>, typically when your average score is around or above the threshold.</li>
+                            </ul>
+                        </li>
                     </ul>
                 </li>
-                <li><b>Days on Track</b>: Reflects the time you've spent on each track. A higher number may indicate a need for more practice or different learning strategies.</li>
-                <li><b>Last Remark</b>: Provides the latest feedback or note on your track performance.</li>
-                <li><b>Assessments</b>: Each track comes with a tailored assessment based on your performance. This includes <b>suggestions and observations</b> aimed at helping you understand your current standing and how to improve. <b>Read these carefully</b> to gain insights into your learning journey.</li>
-                <li><b>Action Steps</b>: <b>Choose a track</b> that aligns with your learning goals. <b>Consistent practice</b> and striving to surpass the threshold scores are crucial for your skill development and mastery of the track.</li>
+                <li><b>Action Steps</b>: <b>Choose a track</b> that aligns with your learning goals. <b>Consistent practice</b> and striving to surpass the threshold scores are crucial for your <b>skill development</b> and mastery of the track.</li>
             </ul>
-            <p><b>Important Note:</b> Once you select a track, make sure to <b>review your recordings and score trends</b> displayed below. Each recording for the selected track will be shown along with scores and reviews. <b>Listen to your own recordings</b> and <b>pay close attention to the reviews</b> to understand the corrections that need to be made. This <b>>self-review</b> process is vital for recognizing areas of improvement and tracking your progress over time.</p>
+            <p><b>Important Note:</b> Once you select a track, make sure to <b>review your recordings</b> and <b>score trends</b> displayed below. Each recording for the selected track will be shown along with scores and reviews. <b>Listen to your own recordings</b> and <b>pay close attention to the reviews</b> to understand the corrections that need to be made. This self-review process is vital for recognizing <b>areas of improvement</b> and tracking your <b>progress</b> over time.</p>
         """, unsafe_allow_html=True)
 
         st.write("")
